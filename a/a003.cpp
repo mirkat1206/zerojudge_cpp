f9dbcd07dca5d28839a50531f082b4b4d2df36f9
Din <mirkat.ee06@g2.nctu.edu.tw>
2020-06-19 23:04:23 +0800

    Update a003.cpp

--- a/a003.cpp
+++ a/a003.cpp
@@ -4,15 +4,14 @@
 int main()
 {
 	int M, D;
-	while( scanf("%d%d", &M , & D)!=EOF )
-	{
-		int S = ( M*2 + D ) % 3 ;
-		if( S==0 )
-			printf("普通\n");
-		else if( S==1 )
-			printf("吉\n");
-		else
-			printf("大吉\n");
-	}
+	scanf("%d%d", &M , & D);
+	int S = ( M*2 + D ) % 3 ;
+	if( S==0 )
+		printf("普通\n");
+	else if( S==1 )
+		printf("吉\n");
+	else
+		printf("大吉\n");
+
 	return 0;
 }
