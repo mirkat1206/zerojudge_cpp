1ff5f7e8fcea3e6e9118218e96bde294e77f0eb8
Din <mirkat.ee06@g2.nctu.edu.tw>
2020-06-20 13:51:57 +0800

    Update a010.cpp

--- a/a010.cpp
+++ a/a010.cpp
@@ -4,34 +4,32 @@
 int main()
 {
 	int input;
-	while( scanf("%d", &input )!=EOF )
+	scanf("%d", &input );
+	int flg = 1 ;
+	for( int i=2 ; i<=input ; i++ )
 	{
-		int flg = 1 ;
-		for( int i=2 ; i<=input ; i++ )
+		int cnt = 0 ;
+		while( input%i == 0 )
 		{
-			int cnt = 0 ;
-			while( input%i == 0 )
-			{
-				cnt++ ;
-				input /= i ;
-			}
-			if( flg && cnt>=1 )
-			{
-				if( cnt>1 )
-					printf("%d^%d", i , cnt );
-				else if( cnt==1 )
-					printf("%d", i );
-				flg = 0 ;
-			}
-			else
-			{
-				if( cnt>1 )
-					printf(" * %d^%d", i , cnt );
-				else if( cnt==1 )
-					printf(" * %d", i );
-			}
+			cnt++ ;
+			input /= i ;
 		}
-		printf("\n");
+		if( flg && cnt>=1 )
+		{
+			if( cnt>1 )
+				printf("%d^%d", i , cnt );
+			else if( cnt==1 )
+				printf("%d", i );
+			flg = 0 ;
+		}
+		else
+		{
+			if( cnt>1 )
+				printf(" * %d^%d", i , cnt );
+			else if( cnt==1 )
+				printf(" * %d", i );
+		}
 	}
+	printf("\n");
 	return 0;
 }
