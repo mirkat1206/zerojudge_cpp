8dc7a711c89099284630331f15db4fe3c8a68667
Din <mirkat.ee06@g2.nctu.edu.tw>
2020-06-20 21:04:29 +0800

    Update a053.cpp

--- a/a053.cpp
+++ a/a053.cpp
@@ -4,16 +4,14 @@
 int main()
 {
 	int n;
-	while( scanf("%d", &n )!=EOF )
-	{
-		if( n>40 )
-			printf("100\n");
-		else if( n>20 )
-			printf("%d\n", 60 + 20 + (n-20)*1 );
-		else if( n>10 )
-			printf("%d\n", 60 + (n-10)*2 );
-		else
-			printf("%d\n", n*6 );
-	}
+	scanf("%d", &n );
+	if( n>40 )
+		printf("100\n");
+	else if( n>20 )
+		printf("%d\n", 60 + 20 + (n-20)*1 );
+	else if( n>10 )
+		printf("%d\n", 60 + (n-10)*2 );
+	else
+		printf("%d\n", n*6 );
 	return 0;
 }
