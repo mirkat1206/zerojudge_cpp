60fb5b6d5cf3ebee940b02bf669792864b61ca53
Din <mirkat.ee06@g2.nctu.edu.tw>
2020-06-20 13:49:15 +0800

    Update a009.cpp

--- a/a009.cpp
+++ a/a009.cpp
@@ -5,12 +5,10 @@
 int main()
 {
 	// printf("input = %d\toutput = %d\n", '1' , '*' );
-	char input[1000000];
-	while( scanf("%s", input )!=EOF)
-	{
-		for( int i=0 ; i<strlen(input) ; i++ )
-			printf("%c", input[i] - 7 );
-		printf("\n");
-	}
+	char input[1000];
+	scanf("%s", input );
+	for( int i=0 ; i<strlen(input) ; i++ )
+		printf("%c", input[i] - 7 );
+	printf("\n");
 	return 0;
 }
